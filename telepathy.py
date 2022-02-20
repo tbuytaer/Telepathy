f5e324faaf4093ca19acee65c719e337e5603de4
Tom Buytaert <24655941+tbuytaer@users.noreply.github.com>
2022-02-20 23:27:46 +0100

    Add argument to skip menu

--- telepathy.py
+++ telepathy.py
@@ -6,6 +6,7 @@ gather memberlists for groups, create edglists for forwarded messages and more.
 
 from telethon.sync import TelegramClient
 from telethon import TelegramClient
+import argparse
 
 __author__ = "Jordan Wildon (@jordanwildon)"
 __license__ = "MIT License"
@@ -14,39 +15,63 @@ __maintainer__ = "Jordan Wildon"
 __email__ = "j.wildon@pm.me"
 __status__ = "Development"
 
-print('Welcome to Telepathy')
-print('Please select a function:')
+""" Get arguments from command line """
+parser = argparse.ArgumentParser(description='Select mode')
+parser.add_argument('--mode', default="menu", choices=['archive', 'archive-period', 'members', 'forwards','userlookup','menu'], help="The action to perform.")
+parser.add_argument('--batch', dest = 'batch', action='store_true', help="Batch mode. Skip questions and don't return to menu afterwards.")
+parser.set_defaults(batch=False)
+args = parser.parse_args()
+mode = args.mode
 
-li = ['Batch chat archiver','Archive within a timeframe','Scrape group members','Scrape forwarded messages in a chat','Lookup a user ID']
-
-def display(li):
-    for idx, tables in enumerate(li):
-        print("%s. %s" % (idx+1, tables))
-
-def get_list(li):
-    choose = int(input("\nPick a number:"))-1
-    if choose < 0 or choose > (len(li)-1):
-        print('Invalid Choice')
-        return ''
-    return li[choose]
-
-display(li)
-choice = (get_list(li))
-
-print('Loading', choice,'...')
-
-if choice == 'Batch chat archiver':
+if mode == 'archive':
     print('Launching batch chat archiver')
     exec(open("archiver.py").read())
-if choice == 'Archive within a timeframe':
+elif mode == 'archive-period':
     print('Launching chat archiver')
     exec(open("timeframe.py").read())
-elif choice == 'Scrape group members':
+elif mode == 'members':
     print('Launching group member scraper...')
     exec(open("members.py").read())
-elif choice == 'Scrape forwarded messages in a chat':
+elif mode == 'forwards':
     print('Launching channel forward scraper...')
     exec(open("forwards.py").read())
-elif choice == 'Lookup a user ID':
+elif mode == 'userlookup':
     print('Launching Telegram user lookup...')
     exec(open("userlookup.py").read())
+elif mode == 'menu':
+    print('Welcome to Telepathy')
+    print('Please select a function:')
+
+    li = ['Batch chat archiver','Archive within a timeframe','Scrape group members','Scrape forwarded messages in a chat','Lookup a user ID']
+
+    def display(li):
+        for idx, tables in enumerate(li):
+            print("%s. %s" % (idx+1, tables))
+
+    def get_list(li):
+        choose = int(input("\nPick a number:"))-1
+        if choose < 0 or choose > (len(li)-1):
+            print('Invalid Choice')
+            return ''
+        return li[choose]
+
+    display(li)
+    choice = (get_list(li))
+
+    print('Loading', choice,'...')
+
+    if choice == 'Batch chat archiver':
+        print('Launching batch chat archiver')
+        exec(open("archiver.py").read())
+    if choice == 'Archive within a timeframe':
+        print('Launching chat archiver')
+        exec(open("timeframe.py").read())
+    elif choice == 'Scrape group members':
+        print('Launching group member scraper...')
+        exec(open("members.py").read())
+    elif choice == 'Scrape forwarded messages in a chat':
+        print('Launching channel forward scraper...')
+        exec(open("forwards.py").read())
+    elif choice == 'Lookup a user ID':
+        print('Launching Telegram user lookup...')
+        exec(open("userlookup.py").read())
